36906d76d40cb7398ec766069ad55565ae53fa9e
stvnsy <127708814+stevenasoy@users.noreply.github.com>
2023-03-24 15:30:57 +0800

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_testing/secondpage.dart';
+
+// function to trigger build when the app is run
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomeRoute(),
+      '/second': (context) => const SecondRoute(),
+      '/third': (context) => const ThirdRoute(),
+    },
+  )); //MaterialApp
+}
+
+class HomeRoute extends StatelessWidget {
+  const HomeRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Geeks for Geeks'),
+        backgroundColor: Colors.green,
+      ), // AppBar
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Click Me!'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/second');
+              },
+            ), // ElevatedButton
+            ElevatedButton(
+              child: const Text('Tap Me!'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/third');
+              },
+            ), // ElevatedButton
+          ], // <Widget>[]
+        ), // Column
+      ), // Center
+    ); // Scaffold
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tap Me Page"),
+        backgroundColor: Colors.green,
+      ), // AppBar
+    ); // Scaffold
+  }
+}
